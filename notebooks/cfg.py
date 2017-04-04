5c348b526b28213ab8387cbd2ff487eb5b82ac4a
fpenovi <avsracing@hotmail.com>
2017-04-04 19:58:38 -0300

    Inicializo notebook root

---
+++ notebooks/cfg.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Path absoluto o relativo a los csv
+STATUS_CSV = '../DataSet/status.csv'
+STATION_CSV = '../DataSet/station.csv'
+TRIP_CSV = '../DataSet/trip.csv'
+WEATHER_CSV = '../DataSet/weather.csv'
